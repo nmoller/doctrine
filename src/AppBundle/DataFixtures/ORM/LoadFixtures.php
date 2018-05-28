21cba8e71c9960da24ad7bda9573fed492fe2e6c
Nelson Moller <nmoller.c@gmail.com>
2018-05-28 16:09:27 -0400

    Adding fixtures and migrations

---
+++ src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nmoller
+ * Date: 28/05/18
+ * Time: 3:19 PM
+ */
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\Genus;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Nelmio\Alice\Fixtures;
+
+
+class LoadFixtures implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        Fixtures::load(
+            __DIR__.'/fixtures.yml',
+            $manager,
+            [
+                'providers' => [$this]
+            ]
+            );
+    }
+
+    public function genus() {
+        $genera = [
+            'Octopus',
+            'Balaena',
+            'Orcinus',
+            'Hippocampus',
+            'Asterias',
+            'Amphipron',
+            'Carcharodon',
+            'Aurelia',
+            'Cucumaria',
+            'Balistoides',
+            'Paralithodes',
+            'Chelonia',
+            'Trichechus',
+            'Eumetopias'
+        ];
+
+        $key = array_rand($genera);
+        return $genera[$key];
+    }
+
+}
